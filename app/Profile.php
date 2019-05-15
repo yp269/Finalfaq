8e3f947bd75485f84225d8f8aed4b5639de80b3b
Yi <yp269@njit.edu>
2019-05-15 17:15:00 -0400

    Feature: Edited profile uploaded

--- app/Profile.php
+++ app/Profile.php
@@ -4,8 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class Profile extends Model
 {
+    protected $fillable = ['fname', 'lname', 'body'];
+
     public function user()
     {
         return $this->belongsTo('App\User');
